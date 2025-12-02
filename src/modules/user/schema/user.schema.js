343c9d66429412c6f03ee4f1f42e2047c742d53f
toia-19 <victoriaangelinadiaz@gmail.com>
2025-12-02 19:40:52 -0300

    doc: Agregué comentario en esquema de usuario

--- src/modules/user/schema/user.schema.js
+++ src/modules/user/schema/user.schema.js
@@ -1,13 +1,8 @@
 import joi from "joi";
 
+/* Esquema de creación de usuario */
 export const createUserDto = joi.object({
     name: joi.string().min(3).required(),
     email: joi.string().email().required(),
     password: joi.string(),
-});
-
-export const updateUserDto = joi.object({
-    name: joi.string().min(3),
-    email: joi.string().email(),
-    password: joi.string().min(6),
 });
